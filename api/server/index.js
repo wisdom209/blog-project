b07f991a57052a2823d9cf0d053fa58e35f9da93
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-25 14:12:54 +0100

    update

--- api/server/index.js
+++ api/server/index.js
@@ -16,12 +16,13 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: ['https://blog-project-nosql.vercel.app/',
-	'https://vercel.com/wisdom209/blog-project-nosql/8tCnNd12TMUkzB7RLyDh2b1V6xpt'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
+	origin: ['https://blog-project-nosql.vercel.app',
+		'https://vercel.com/wisdom209/blog-project-nosql/8tCnNd12TMUkzB7RLyDh2b1V6xpt'],
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+	credentials: true
 }))
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
