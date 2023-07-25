188a2ef23fcfbbc0cdb6b43c0f10ac3b712cd220
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-25 10:25:52 +0100

    update prod

--- api/server/index.js
+++ api/server/index.js
@@ -12,23 +12,22 @@ const app = express();
 dotenv.config();
 
 app.use(cookieParser())
-app.use(cors({ origin: ['https://blog-project-nosql.vercel.app', 'https://vercel.com/wisdom209/blog-project-nosql/G2nNkQMY6stQPWwq51EJiAeDP3Zi'], credentials: true }));
+app.use(cors({ origin: ['https://blog-project-nosql.vercel.app', 'https://vercel.com/wisdom209/blog-project-nosql/HrgPKmRLBsj6g3ibHy1nZMHu3vJW'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(userRouter)
 app.use(postRouter)
 
+
 /* CONNECT TO DB AND START SERVER */
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((response => {
-		console.log(process.env.JWT_SECRET)
 		console.log('connected')
 		app.listen(PORT, () => {
 			console.log(`Server: ${PORT}`);
 		});
 	}))
 	.catch(error => {
-
 		console.log(error)
 	})
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		watch: { usePolling: true },
+		host: true,
+		strictPort: true,
+		port: 5173,
+	},preview: {port: 5173, host: true, strictPort: true }
 })
